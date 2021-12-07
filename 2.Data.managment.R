2c167c6bc0650f967044c6c9ad7e1af10ab82550
sevsant <94870812+sevsant@users.noreply.github.com>
2021-12-06 22:48:13 -0800

    Update 2.Data.managment.R

--- 2.Data.managment.R
+++ 2.Data.managment.R
@@ -1,7 +1,6 @@
 # read csv files for stock exchange indices 
 ## we have to change this so it reads from the RAWdata Folder, 
 ## but I couldnt figure it out
-library(dplyr)
 
 
 list.files()
@@ -9,6 +8,7 @@ csv.SP500 <- read.csv("SP500.5Years.csv")
 csv.NASDAQ <- read.csv("NASDAQ.5Years.csv")
 csv.DJI <- read.csv("DowJones.5Years.csv")
 csv.TSX <- read.csv("tsx_composite_index_canada.csv")
+csv.DAX <- read.csv("DAX.5Years.csv")
 
 # data frame check for each csv file
 head(csv.SP500)
@@ -19,22 +19,22 @@ head(csv.DJI)
 str(csv.DJI)
 head(csv.TSX)
 str(csv.TSX)
+head(csv.DAX)
+str(csv.DAX)
 
 # read csv files for covid19 data 
-## we have to change this so it reads from the RAWdata Folder, 
-## but I couldnt figure it out
 list.files()
 csv.CVD.CAN <- read.csv("covid19canada-download.csv")
 csv.CVD.USA <- read.csv("CoronaUSDec12.csv")
+csv.CVD.DE <- read.csv("cases-rki-by-state_germany.csv")
 
 # data frame check for each csv file
 head (csv.CVD.CAN)
 str(csv.CVD.CAN)
 head (csv.CVD.USA)
 str(csv.CVD.USA)
-
-# sorting covid data from Canada by whole country
-Canada.covid.data <- csv.CVD.CAN[csv.CVD.CAN$prname == "Canada",]
+head(csv.CVD.DE)
+str(csv.CVD.DE)
 
 # create function to find start and stop dates for data 
 find.dates <- function(date) {
@@ -55,85 +55,164 @@ alter.date.fmt.2 <- function(date){
   dates.cr.fmt.2 <- as.Date.character(date, format = "%b %d,%Y")
   return(dates.cr.fmt.2)
 }
+# remove comma from DJI and DAX values
+csv.DJI$Price <- as.numeric(gsub(",","",csv.DJI$Price))
+csv.DAX$Price <- as.numeric(gsub(",","",csv.DAX$Price))
 
-#  format = "%b %d,%Y"
 
-# creating vectors of equal length, finding start and stop dates for covid data
-# Canada
-Canada.dates <- csv.CVD.CAN$date
-Canada.CVD.dates.max.min <- find.dates(Canada.covid.data$date)
 
-# USA
+# sorting covid data from Canada by whole country
+CAN.CVD.data <- csv.CVD.CAN[csv.CVD.CAN$prname == "Canada",]
+
+# create data frame from Canada covid daily cases and dates 
+CAN.CVD.df <- cbind.data.frame(CAN.CVD.data$date, CAN.CVD.data$numtoday)
+
 # replace / in submitted dates with - for correct reading of calendar values
 USA.covid.cr.dates <- alter.date.fmt(csv.CVD.USA$submission_date)
 csv.CVD.USA$submission_date <- USA.covid.cr.dates
 
-# 
+# create dataframe with daily covid cases for all states in USA
 csv.CVD.USA$new_case <- as.numeric(gsub(",","",csv.CVD.USA$new_case))
-USA.covid.data <- aggregate(csv.CVD.USA['new_case'], by=csv.CVD.USA['submission_date'], sum)
+USA.CVD.df <- aggregate(csv.CVD.USA['new_case'], by=csv.CVD.USA['submission_date'], sum)
 
-USA.dates <- find.dates(csv.CVD.USA$submission_date)
+# remove time from dates in Germany covid data
+DE.covid.cr.dates <- gsub(csv.CVD.DE$time_iso8601,pattern="T17:00:00+0000",replacement="",fixed=T)
+csv.CVD.DE$time_iso8601 <- DE.covid.cr.dates
 
-# creating vectors of equal length, finding start and stop dates for indecis data
-# SP500
+
+# create vector for daily cases by finding difference between total cases day (n) - (n-1)
+DE.daily.cases <- ave(csv.CVD.DE$sum_cases, FUN = function(x) c(0,diff(x)))
+# replace first value in vector with original total case value from csv file
+DE.daily.cases <- replace(DE.daily.cases, DE.daily.cases==0,csv.CVD.DE$sum_cases[1])
+# create dataframe with daily covid cases from all states in Germany
+DE.CVD.df <- cbind.data.frame(csv.CVD.DE$time_iso8601,DE.daily.cases)
+
+
+# Finding start and stop dates for covid data
+# Canada
+CAN.CVD.dates.min.max <- find.dates(CAN.CVD.df$`CAN.CVD.data$date`)
+
+# USA
+USA.CVD.dates.min.max <- find.dates(USA.CVD.df$submission_date)
+
+# Germany
+DE.CVD.dates.min.max <- find.dates(DE.CVD.df$`csv.CVD.DE$time_iso8601`)
+
+
+
 # replace / in submitted dates with - for correct reading of calendar values
-SP500.cr.date.fmt <- alter.date.fmt(csv.SP500$Date)
-csv.SP500$Date <- SP500.cr.date.fmt
-
-# SP500 dates
-
-find.dates(csv.SP500$Date)
-
+# SP500
+SP500.cr.fmt.date <- alter.date.fmt(csv.SP500$Date)
+csv.SP500$Date <- SP500.cr.fmt.date
 
 # NASDAQ 
-find.dates(csv.NASDAQ$Date)
 
 # DJI
-DJI.dates <- csv.DJI$Date
-DJI.cr.dates <- alter.date.fmt.2(csv.DJI$Date)
-csv.DJI$Date <- DJI.cr.dates
-
-find.dates(csv.DJI$Date)
+DJI.cr.fmt.date <- alter.date.fmt.2(csv.DJI$Date)
+csv.DJI$Date <- DJI.cr.fmt.date
 
 # TSX
-find.dates(csv.TSX$Date)
+
+# DAX
+DAX.cr.fmt.date <- alter.date.fmt.2(csv.DAX$Date)
+csv.DAX$Date <- DAX.cr.fmt.date
+
+# create vectors for daily closing values from each index by USA covid dates
+# SP500
+SP500.dates <- csv.SP500$Date[csv.SP500$Date >= USA.CVD.dates.min.max[1] & csv.SP500$Date <= USA.CVD.dates.min.max[2]]
+SP500.close <- csv.SP500$Close.Last[csv.SP500$Date >= USA.CVD.dates.min.max[1] & csv.SP500$Date <= USA.CVD.dates.min.max[2]]
+
+# NASDAQ
+NASDAQ.dates <- csv.NASDAQ$Date[csv.NASDAQ$Date >= USA.CVD.dates.min.max[1] & csv.NASDAQ$Date <= USA.CVD.dates.min.max[2]]
+NASDAQ.close <- csv.NASDAQ$Close[csv.NASDAQ$Date >= USA.CVD.dates.min.max[1] & csv.NASDAQ$Date <= USA.CVD.dates.min.max[2]]
+
+# DJI
+DJI.dates <- csv.DJI$Date[csv.DJI$Date >= USA.CVD.dates.min.max[1] & csv.DJI$Date <= USA.CVD.dates.min.max[2]]
+DJI.close <- csv.DJI$Price[csv.DJI$Date >= USA.CVD.dates.min.max[1] & csv.DJI$Date <= USA.CVD.dates.min.max[2]]
 
 # create vectors for daily closing values from each index by Canada covid dates
-SP500.dates <- csv.SP500$Date[csv.SP500$Date >= Canada.CVD.dates.max.min[1] & csv.SP500$Date <= Canada.CVD.dates.max.min[2]]
-SP500.close <- csv.SP500$Close.Last[csv.SP500$Date >= Canada.CVD.dates.max.min[1] & csv.SP500$Date <= Canada.CVD.dates.max.min[2]]
+# TSX
+TSX.dates <- csv.TSX$Date[csv.TSX$Date >= CAN.CVD.dates.min.max[1] & csv.TSX$Date <= CAN.CVD.dates.min.max[2]]
+TSX.close <- csv.TSX$Close[csv.TSX$Date >= CAN.CVD.dates.min.max[1] & csv.TSX$Date <= CAN.CVD.dates.min.max[2]]
 
-# create data frame for daily closing values from each index by Canada covid dates
-SP500.df <- cbind.data.frame(SP500.dates,SP500.close)
-#sp500.close.df <- cbind.data.frame(close.SP500)
+# DAX
+DAX.dates <- csv.DAX$Date[csv.DAX$Date >= DE.CVD.dates.min.max[1] & csv.DAX$Date <= DE.CVD.dates.min.max[2]]
+DAX.close <- csv.DAX$Price[csv.DAX$Date >= DE.CVD.dates.min.max[1] & csv.DAX$Date <= DE.CVD.dates.min.max[2]]
 
+# create data frame for daily closing values from each index by associated covid data
+# SP500
+SP500.df <- cbind.data.frame(SP500.dates, SP500.close)
 
-#close.NASDAQ <- csv.NASDAQ$Close[csv.NASDAQ$Date >= Canada.dates[1] & csv.NASDAQ$Date <= Canada.dates[2]]
+# NASDAQ
+NASDAQ.df <- cbind.data.frame(NASDAQ.dates, NASDAQ.close)
 
-#close.DJI <- csv.DJI$Price[csv.DJI$Date >= Canada.dates[1] & csv.DJI$Date <= Canada.dates[2]]
+# DJI
+DJI.df <- cbind.data.frame(DJI.dates, DJI.close)
 
-#close.TSX <- csv.TSX$Close[csv.TSX$Date >= Canada.dates[1] & csv.TSX$Date <= Canada.dates[2]]
+# TSX
+TSX.df <- cbind.data.frame(TSX.dates, TSX.close)
 
-
-# create vectors for daily cases in each country 
-cases.d.CAN <- Canada.covid.data$numtoday
-cases.d.USA <- USA.covid.data$new_case
-
-# create data frame from Canada covid daily cases and dates 
-Canada.Cases.df <- cbind.data.frame(Canada.covid.data$date, Canada.covid.data$numtoday)
+# DAX
+DAX.df <- cbind.data.frame(DAX.dates, DAX.close)
 
 # rename column names for all data frame dates to "Date"
-names(Canada.Cases.df)[names(Canada.Cases.df) == "Canada.covid.data$date"] <- "Date"
+# countries 
+names(CAN.CVD.df)[names(CAN.CVD.df) == "CAN.CVD.data$date"] <- "Date"
+names(USA.CVD.df)[names(USA.CVD.df) == "submission_date"] <- "Date"
+names(DE.CVD.df)[names(DE.CVD.df) == "csv.CVD.DE$time_iso8601"] <- "Date"
+
+# indices 
 names(SP500.df)[names(SP500.df) == "SP500.dates"] <- "Date"
+names(NASDAQ.df)[names(NASDAQ.df) == "NASDAQ.dates"] <- "Date"
+names(DJI.df)[names(DJI.df) == "DJI.dates"] <- "Date"
+names(TSX.df)[names(TSX.df) == "TSX.dates"] <- "Date"
+names(DAX.df)[names(DAX.df) == "DAX.dates"] <- "Date"
 
-# cast values in Date columnes as Dates 
-Canada.Cases.df$Date <- as.Date(Canada.Cases.df$Date)
+# rename column names for country data frames to "Daily cases"
+names(CAN.CVD.df)[names(CAN.CVD.df) == "CAN.CVD.data$numtoday"] <- "Daily cases"
+names(USA.CVD.df)[names(USA.CVD.df) == "new_case"] <- "Daily cases"
+names(DE.CVD.df)[names(DE.CVD.df) == "DE.daily.cases"] <- "Daily cases"
+
+# rename column names for index data frames to "Closing price"
+names(SP500.df)[names(SP500.df) == "SP500.close"] <- "Closing price"
+names(NASDAQ.df)[names(NASDAQ.df) == "NASDAQ.close"] <- "Closing price"
+names(DJI.df)[names(DJI.df) == "DJI.close"] <- "Closing price"
+names(TSX.df)[names(TSX.df) == "TSX.close"] <- "Closing price"
+names(DAX.df)[names(DAX.df) == "DAX.close"] <- "Closing price"
+
+# cast values in Date columns as Dates 
+# countries 
+CAN.CVD.df$Date <- as.Date(CAN.CVD.df$Date)
+USA.CVD.df$Date <- as.Date(USA.CVD.df$Date)
+DE.CVD.df$Date <- as.Date(DE.CVD.df$Date)
+# indices
 SP500.df$Date <- as.Date(SP500.df$Date)
+NASDAQ.df$Date <- as.Date(NASDAQ.df$Date)
+DJI.df$Date <- as.Date(DJI.df$Date)
+TSX.df$Date <- as.Date(TSX.df$Date)
+DAX.df$Date <- as.Date(DAX.df$Date)
 
-# inner join merge of data frames such that dates with values match
-CAN.SP500.merged <- merge(x= Canada.Cases.df, y=SP500.df, by = 'Date')
 
-## test correlation, works yay!!
-cor(CAN.SP500.merged$SP500.close, CAN.SP500.merged$`Canada.covid.data$numtoday`)
+# inner join merge of data frames for each country such that equal dates with values match
+# USA
+USA.SP500.merged <- merge(x= USA.CVD.df, y=SP500.df, by = 'Date')
+USA.NASDAQ.merged <- merge(x= USA.CVD.df, y=NASDAQ.df, by = 'Date')
+USA.DJI.merged <- merge(x= USA.CVD.df, y=DJI.df, by = 'Date')
+# Canada
+CAN.TSX.merged <- merge(x= CAN.CVD.df, y=TSX.df, by = 'Date')
+# Germany
+DE.DAX.merged <- merge(x= DE.CVD.df, y=DAX.df, by = 'Date')
+
+
+# test correlation 
+# USA
+cor(USA.SP500.merged$`Closing price`, USA.SP500.merged$`Daily cases`)
+cor(USA.NASDAQ.merged$`Closing price`, USA.NASDAQ.merged$`Daily cases`)
+cor(USA.DJI.merged$`Closing price`, USA.NASDAQ.merged$`Daily cases`)
+# Canada
+cor(CAN.TSX.merged$`Closing price`, CAN.TSX.merged$`Daily cases`)
+# Germany
+cor(DE.DAX.merged$`Closing price`, DE.DAX.merged$`Daily cases`)
 
 
 
